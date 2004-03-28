368ea399252e560ac8e0f936613f3f520c1cf8a7
sky <sky>
2004-03-28 23:01:27 +0000

    - TV::TV(): additions

--- transvader/tv.cxx
+++ transvader/tv.cxx
@@ -9,6 +9,7 @@
 
 TV::TV()
 {
+	/* initialize Allegro */
 	if ( install_allegro ( SYSTEM_AUTODETECT, &errno, atexit ) )
 	{
 		char* errmsg = new char[200];
@@ -18,9 +19,33 @@ TV::TV()
 		throw Exception(errmsg, ERR_FATAL);
 	}
 	
+	/* init graphics mode */
+	if ( set_gfx_mode ( GFX_AUTODETECT_WINDOWED, 800, 600, 0, 0 ) < 0 )
+	{
+		char* errmsg = new char[200];
+		
+		sprintf(errmsg, "Could not set graphics mode (%s)",
+			allegro_error );
+			
+		throw Exception(errmsg, ERR_FATAL);
+	}
+
+	/* set configuration variables */
 	set_window_title("Transvader-DEVEL");
+	set_display_switch_mode ( SWITCH_PAUSE );
+	if ( !enable_triple_buffer() )
+	{
+		allegro_message ( "Triple buffering enabled\n" );
+	}
 	
+	/* init keyboard, timer and sound */
+	install_keyboard();
+	install_timer();
+	install_sound ( DIGI_AUTODETECT, MIDI_AUTODETECT, NULL );
 	
+	set_color_depth(16);
+	
+	readkey();
 	
 	return;
 }
