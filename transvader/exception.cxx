359f1758ade53cb116890a0b78316ce0299367a4
fkastl <fkastl>
2004-04-06 11:20:33 +0000

    - Added DISCLAIMER (Copyright notice) - Added copyright lines and references to DISCLAIMER and LICENSE to every source- and makefile

--- transvader/exception.cxx
+++ transvader/exception.cxx
@@ -1,3 +1,9 @@
+// This file is a part of the Transvader sourcecode. Transvader is a free Asteroids clone distributed under GPL
+// Copyright (C) 2004  Leslie P. Polzer
+// The full copyright notice can be found in the file DISCLAIMER at the root of this distribution.
+// This program is distributed under the GNU General Public License, for details read the file LICENSE 
+// at the root of this distribution
+
 #include <stdlib.h>		/* abort() */
 #include <string.h>
 #include <stdio.h>
---
+++ transvader/sprite.cxx
@@ -0,0 +1,5 @@
+// This file is a part of the Transvader sourcecode. Transvader is a free Asteroids clone distributed under GPL
+// Copyright (C) 2004  Leslie P. Polzer
+// The full copyright notice can be found in the file DISCLAIMER at the root of this distribution.
+// This program is distributed under the GNU General Public License, for details read the file LICENSE 
+// at the root of this distribution
