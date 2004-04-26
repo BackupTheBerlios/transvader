c171af5e56971ff5cf623ceaf471353c5517dd2e
fkastl <fkastl>
2004-04-26 16:28:04 +0000

    - This file should grow... so... Don't Panic

---
+++ transvader/types.hxx
@@ -0,0 +1,9 @@
+// This file is a part of the Transvader sourcecode. Transvader is a free Asteroids clone distributed under GPL
+// Copyright (C) 2004  Ferdinand Kastl
+// The full copyright notice can be found in the file DISCLAIMER at the root of this distribution.
+// This program is distributed under the GNU General Public License, for details read the file LICENSE 
+// at the root of this distribution
+struct POINT
+{
+	short x,y;
+};
