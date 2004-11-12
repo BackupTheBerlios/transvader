d8101b0cc73b8a9041f8b17926bf6f95c9310ced
sky <sky>
2004-11-12 18:03:35 +0000

    Added some minor objects

---
+++ transvader2/include/pilot.hxx
@@ -0,0 +1,21 @@
+#ifndef PILOT_HXX
+#define PILOT_HXX
+
+
+namespace TV
+{
+
+class Pilot
+{
+	public:
+		Pilot();
+		~Pilot();
+	
+	
+	protected:
+		/* pilot properties here */
+};
+
+} /* namespace TV */
+
+#endif
---
+++ transvader2/include/ship.hxx
@@ -0,0 +1,21 @@
+#ifndef SHIP_HXX
+#define SHIP_HXX
+
+
+namespace TV
+{
+
+class Ship
+{
+	public:
+		Ship();
+		~Ship();
+	
+	
+	protected:
+		/* ship properties here */
+};
+
+} /* namespace TV */
+
+#endif
